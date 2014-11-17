76f06141c061da8a953bbe2af49c801fa4a9e721
macbric <macbric>
2014-11-17 15:23:45 -0500

    Création de l'objet Solution

---
+++ src/com/udem/ift6243/model/Solution.java
@@ -0,0 +1,31 @@
+package com.udem.ift6243.model;
+
+public class Solution
+{
+	private Integer id;
+	private Integer categoryId;
+	private String name;
+	private String description;
+	
+	public Solution(Integer id, Integer categoryId, 
+			String name, String description) {
+		super();
+		this.id = id;
+		this.categoryId = categoryId;
+		this.name = name;
+		this.description = description;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
+}
