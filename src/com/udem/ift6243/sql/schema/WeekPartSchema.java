92fa12b269dc0d41c3dee1950d9adc80eb2b9483
macbric <macbric>
2014-11-17 20:06:13 -0500

    Création du schéma sql pour les parties (3) d'une semaine

---
+++ src/com/udem/ift6243/sql/schema/WeekPartSchema.java
@@ -0,0 +1,39 @@
+package com.udem.ift6243.sql.schema;
+
+import java.util.ArrayList;
+
+public class WeekPartSchema
+{
+	public static final String TABLE_NAME = "week_part";
+	
+	public static final String TABLE_COL_ID = "id";
+	public static final String TABLE_COL_NAME = "name";
+	public static final String TABLE_COL_START_HOUR = "start_day";
+	public static final String TABLE_COL_END_HOUR = "end_day";
+	
+	public static String getCreateTableSchema()
+	{
+		return "CREATE TABLE `" + TABLE_NAME + "` ("
+			+ "`id`	INTEGER PRIMARY KEY AUTOINCREMENT,"
+			+ "`name`	TEXT UNIQUE,"
+			+ "`start_day`	INTEGER,"
+			+ "`end_day`	INTEGER"
+			+ ");";
+	}
+	
+	public static ArrayList<String> getInsertDataList()
+	{
+		ArrayList<String> queryList = new ArrayList<String>();
+		
+		queryList.add("INSERT INTO " + TABLE_NAME + " VALUES (NULL, 'Début', 0, 3);"); // Lundi a jeudi
+		queryList.add("INSERT INTO " + TABLE_NAME + " VALUES (NULL, 'Milieu', 4, 4);"); // vendredi
+		queryList.add("INSERT INTO " + TABLE_NAME + " VALUES (NULL, 'Fin', 5, 6);"); // samedi et dimanche
+		
+		return queryList;
+	}
+	
+	public static String getDropTableSchema()
+	{
+		return "DROP TABLE IF EXISTS `" + TABLE_NAME + "`;";
+	}
+}
