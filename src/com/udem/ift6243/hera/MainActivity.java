bc009a3dde78d13b19a147b79cc20b704c27e177
ImeneEG2014 <ielghazzani@gmail.com>
2014-11-19 15:55:56 -0500

    tentative de retrait du bouton

--- src/com/udem/ift6243/hera/MainActivity.java
+++ src/com/udem/ift6243/hera/MainActivity.java
@@ -1,12 +1,9 @@
 package com.udem.ift6243.hera;
 
-
-
 import com.udem.ift6243.dao.DatabaseHandler;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,20 +12,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
 
-	private Button btnForm;
-	
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		btnForm = (Button) findViewById(R.id.button1);
-	    btnForm.setOnClickListener(this);
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -93,12 +83,5 @@ public class MainActivity extends Activity implements OnClickListener {
 					false);
 			return rootView;
 		}
-	}
-
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		Intent i = new Intent(getApplicationContext(),UserFormActivity.class);
-  	    startActivity(i);
 	}
 }
