5632aee506c7675360690b7540c1603adc16eb01
macbric <macbric>
2014-11-23 13:39:07 -0500

    Ajout d'une méthode permettant de récupérer toutes les solutions

--- src/com/udem/ift6243/dao/SolutionDao.java
+++ src/com/udem/ift6243/dao/SolutionDao.java
@@ -1,5 +1,7 @@
 package com.udem.ift6243.dao;
 
+import java.util.ArrayList;
+
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -58,5 +60,48 @@ public class SolutionDao
 		}
 		
 		return solution;
+	}
+	
+	public ArrayList<Solution> getSolution()
+	{
+		ArrayList<Solution> solutionList = new ArrayList<Solution>();
+		Solution solution = null;
+		
+		DatabaseHandler dbHandler = new DatabaseHandler(this.context);
+		SQLiteDatabase db = dbHandler.getReadableDatabase();
+		Cursor cursor_solution = null;
+		
+		db.beginTransaction();
+		try
+		{
+			cursor_solution = db.query(SolutionSchema.TABLE_NAME, 
+					null, null, null, null, null, null, null);
+
+			if (cursor_solution != null && cursor_solution.moveToFirst())
+			{
+	            while (cursor_solution.isAfterLast() == false)
+	            {
+	            	Integer solutionId = Integer.valueOf(cursor_solution.getInt(0));
+					Integer categoryId = Integer.valueOf(cursor_solution.getInt(1));
+					String name = cursor_solution.getString(2);
+					String description = cursor_solution.getString(3);
+					
+					solution = new Solution(solutionId, categoryId, name, description);
+	                
+					solutionList.add(solution);
+	                cursor_solution.moveToNext();
+	            }
+	        }
+			
+			db.setTransactionSuccessful();
+		}
+		finally
+		{
+			db.endTransaction();
+			db.close();
+			if(cursor_solution != null) cursor_solution.close();
+		}
+		
+		return solutionList;
 	}
 }
