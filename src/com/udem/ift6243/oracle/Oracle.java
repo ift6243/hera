ebe5c4cf1349c8c8071e88cb7cec33eebe276b49
macbric <macbric>
2014-11-23 13:39:41 -0500

    Ajout méthode de recherche d'une bonne solution (random) version 1.

--- src/com/udem/ift6243/oracle/Oracle.java
+++ src/com/udem/ift6243/oracle/Oracle.java
@@ -1,9 +1,18 @@
 package com.udem.ift6243.oracle;
 
+import java.util.ArrayList;
+import java.util.Random;
+
+import android.content.Context;
+
+import com.udem.ift6243.dao.SolutionDao;
+import com.udem.ift6243.model.Solution;
+
 public final class Oracle
 {
     private static volatile Oracle instance = null;
-
+    private static Context context;
+    
     /**
      * Constructeur de l'objet.
      */
@@ -24,5 +33,42 @@ public final class Oracle
            }
         }
         return Oracle.instance;
+    }
+    
+    /**
+     * Initialize context
+     * @param context
+     */
+    public static void setContext(Context context)
+    {
+    	Oracle.context = context;
+    }
+    
+    /**
+     * Demarrage de l'Oracle
+     */
+    public static void start()
+    {
+    	Solution solution = Oracle.findSolution();
+    	
+    	if(solution != null)
+    	{
+    		//// LANCEMENT DE LA NOTIFICATION
+    	}
+    }
+    
+    private static Solution findSolution()
+    {
+    	Solution solution = null;
+    	
+    	SolutionDao solutionDao = new SolutionDao(Oracle.context);
+    	ArrayList<Solution> solutionList = solutionDao.getSolution();
+    	
+    	
+    	Random r = new Random();
+    	int selectedIndex = r.nextInt(solutionList.size());
+    	solution = solutionList.get(selectedIndex);
+    	
+    	return solution;
     }
 }
