6eef3e071b9214b5d0b77777ef9e95fa15cefcea
macbric <macbric>
2014-12-01 22:06:48 -0500

    Nouvelle méthode pour récupérer les solutions en fonction du contexte

--- src/com/udem/ift6243/dao/SolutionProposedDao.java
+++ src/com/udem/ift6243/dao/SolutionProposedDao.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import com.udem.ift6243.model.HeraContext;
 import com.udem.ift6243.model.SolutionProposed;
 import com.udem.ift6243.sql.schema.SolutionProposedSchema;
 
@@ -54,6 +55,51 @@ public class SolutionProposedDao
 		}
 		
 		return !error;
+	}
+	
+	public ArrayList<SolutionProposed> getSolutionProposedFromHeraContext(HeraContext heraContext)
+	{
+		ArrayList<SolutionProposed> solutionProposedList = new ArrayList<SolutionProposed>();
+		SolutionProposed solutionProposed = null;
+		
+		DatabaseHandler dbHandler = new DatabaseHandler(this.context);
+		SQLiteDatabase db = dbHandler.getReadableDatabase();
+		Cursor cursor_solution_proposed = null;
+		
+		db.beginTransaction();
+		try
+		{
+			cursor_solution_proposed = db.query(SolutionProposedSchema.TABLE_NAME, 
+					null, null, null, null, null, null, null);
+
+			if (cursor_solution_proposed != null && cursor_solution_proposed.moveToFirst())
+			{
+	            while (cursor_solution_proposed.isAfterLast() == false)
+	            {
+	            	Integer solutionProposedId = Integer.valueOf(cursor_solution_proposed.getInt(0));
+	        		Integer heraContextId = Integer.valueOf(cursor_solution_proposed.getInt(1));
+	            	Integer solutionId = Integer.valueOf(cursor_solution_proposed.getInt(2));
+	        		Double bonus = Double.valueOf(cursor_solution_proposed.getDouble(3));
+	        		String date = cursor_solution_proposed.getString(4);
+	        		
+	        		solutionProposed = new SolutionProposed(solutionProposedId, 
+	        				heraContextId, solutionId, bonus, date);
+	        		
+					solutionProposedList.add(solutionProposed);
+	                cursor_solution_proposed.moveToNext();
+	            }
+	        }
+			
+			db.setTransactionSuccessful();
+		}
+		finally
+		{
+			db.endTransaction();
+			db.close();
+			if(cursor_solution_proposed != null) cursor_solution_proposed.close();
+		}
+		
+		return solutionProposedList;
 	}
 	
 	public ArrayList<SolutionProposed> getSolutionProposed()
